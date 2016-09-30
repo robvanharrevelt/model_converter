1bda5a07ae0534c70f700dbc01a7dc3c936b0ac5
Rob van Harrevelt <rvh@cpb.nl>
2016-09-30 16:08:20 +0200

    Removed mdlcompiler_parsetab.py from the repository

---
+++ islm/convert_mdl.sh
@@ -0,0 +1,4 @@
+PYTHON=/home/rvh/anaconda3/bin/python
+$PYTHON ../convert_mdl.py islm.mdl --output-type=C
+$PYTHON ../convert_mdl.py islm.mdl --output-type=R
+$PYTHON ../convert_mdl.py islm.mdl --output-type=Python
